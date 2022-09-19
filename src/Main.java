eee3723c6994d6f9dc5620b25d3a21abdd8cc906
Long <nguye3lg@mail.uc.edu>
2022-09-18 22:34:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num=7;
+
+        if (num % 2 == 0)
+        {
+            System.out.println("The number " + num + " is an even number");
+        }
+        else
+        {
+            System.out.println("The number " + num + " is an odd number");
+        }
+
+    }
+}
